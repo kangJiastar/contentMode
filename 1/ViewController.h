3e5b0e34392db3c385d70a7d36d77762eed692cc
kangjiastar <168568@123.com>
2015-07-06 09:59:52 +0800

    Initial Commit

---
+++ 1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1
+//
+//  Created by apple on 15/7/6.
+//  Copyright (c) 2015年 cloud7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1
+//
+//  Created by apple on 15/7/6.
+//  Copyright (c) 2015年 cloud7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
